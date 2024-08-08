b5cfab17f6bcdbbbf3e4ba8edf88e6c839c9d865
Emmanuel Aina <emmanuel.aina@bulldogs.aamu.edu>
2024-08-08 09:24:29 -0400

    new10

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import {Box, Stack, Typography, Button, Modal, TextField} from '@mui/material' 
 import { firestore } from '@/firebase'
 import { collection, doc, getDocs, query, setDoc, deleteDoc, getDoc,} from 'firebase/firestore'
